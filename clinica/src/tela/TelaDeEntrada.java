9bf4d527290542cf7bd26682baf8b065581ef41e
Matheus Roks <matheusroksgomesrodrigues@gmail.com>
2025-07-26 12:03:31 -0400

    update

---
+++ clinica/src/tela/TelaDeEntrada.java
@@ -0,0 +1,9 @@
+package tela;
+
+public class TelaDeEntrada {
+	// Os comentários são feitos assim. Apesar de estar aparecendo como erro
+	public static void main(String[] args) {
+		System.out.println("Olha que divertido");
+		
+	}
+}
