2dad8f79fa5535874965986840788a4963f0f242
Matheus Roks <matheusroksgomesrodrigues@gmail.com>
2025-07-26 14:02:33 -0400

    atualização

--- exercicios/src/fundamentos/AreaCircunferencia.java
+++ exercicios/src/fundamentos/AreaCircunferencia.java
@@ -2,6 +2,25 @@ package fundamentos;
 
 public class AreaCircunferencia {
 	public static void main(String[] args) {
-		int raio = 3;
+		double raio = 3;
+		final double PI = 3.14159;
+		double area = raio * raio *PI;
+		
+		/*
+		 * em Java há a diferenciação entre uma constante e uma variável.
+		 * Ao definir uma variável o primeiro dado é: tipo -> nome -> atribuição;
+		 * já ao definir uma constante, muda levemente: final tipo -> nome -> atribuição;
+		 * o final garante que minha constante não seja modificada durante o decorrer do código.
+		 * ademais, por convenção em java, constantes são escritas totalmente em maiusculo;
+		 * alguns tipos já sabidos:
+		 * int: valores inteiros;
+		 * double: os float;
+		 */
+		
+		System.out.println(area);
+		raio = 10;
+		System.out.println("Caso alteremos o valor do raio para 10 teremos");
+		area = raio * raio *PI;
+		System.out.println("Área= " + area);
 	}
 }
--- exercicios/src/fundamentos/PrimeiroProjeto.java
+++ exercicios/src/fundamentos/PrimeiroProjeto.java
@@ -1,5 +1,7 @@
 package fundamentos;
-
+/**@author Matheus Roks
+ * 
+ */
 public class PrimeiroProjeto {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
